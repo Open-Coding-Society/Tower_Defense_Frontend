13e400146d5afc9d927484f1d75ae9b3cd97370d
LarsLindain <andre.lindain@gmail.com>
2025-05-20 10:21:53 -0700

    co poilot uzz needs help

--- thewall.js
+++ thewall.js
@@ -1,13 +1,13 @@
 // triple the defense budget 🚨🚨🚨
 
 
-imageSrc: 'https://i.postimg.cc/TPZstVyP/image-2025-05-20-095201612.png' // 2 man with archers
-imageSrc: 'https://i.postimg.cc/sx8GWg6b/image-2025-05-20-095324671.png' // wizard tower function
-imageSrc: 'https://i.postimg.cc/Y9vWCF8Q/image-2025-05-20-095600055.png' // inferno tower function
-imageSrc: 'https://i.postimg.cc/ZKtdJCNy/image-2025-05-20-095705631.png' // tesluzz
-imageSrc: 'https://i.postimg.cc/JhnypXfS/image-2025-05-20-095832932.png' // bomb party
-imageSrc: 'https://i.postimg.cc/sDmDbX4z/image-2025-05-20-100137225.png' // magic tower lookin ahh
-imageSrc: 'https://i.postimg.cc/jqQsJ59f/image-2025-05-20-100226131.png' // simon says freeze 
-imageSrc: 'https://i.postimg.cc/P5chk8Lg/image-2025-05-20-100344794.png' // hunter yn
-imageSrc: 'https://i.postimg.cc/br3cVGcF/image-2025-05-20-100433723.png' // 6'2" lighting
-imageSrc: 'https://i.postimg.cc/PfKgj89S/image-2025-05-20-100521354.png' // ragussy
+imageSrc: 'https://i.postimg.cc/TPZstVyP/image-2025-05-20-095201612.png' // Archer Tower; 2 man with archers
+imageSrc: 'https://i.postimg.cc/sx8GWg6b/image-2025-05-20-095324671.png' // Wizard Tower; wizard tower function
+imageSrc: 'https://i.postimg.cc/Y9vWCF8Q/image-2025-05-20-095600055.png' // Inferno Tower; inferno tower function
+imageSrc: 'https://i.postimg.cc/ZKtdJCNy/image-2025-05-20-095705631.png' // Tesla Coil; tesluzz
+imageSrc: 'https://i.postimg.cc/JhnypXfS/image-2025-05-20-095832932.png' // Bomb Tower; bomb party
+imageSrc: 'https://i.postimg.cc/sDmDbX4z/image-2025-05-20-100137225.png' // Magic Beam Tower; magic tower lookin ahh
+imageSrc: 'https://i.postimg.cc/jqQsJ59f/image-2025-05-20-100226131.png' // Freeze Tower; simon says freeze 
+imageSrc: 'https://i.postimg.cc/P5chk8Lg/image-2025-05-20-100344794.png' // Hunter Tower; hunter yn
+imageSrc: 'https://i.postimg.cc/br3cVGcF/image-2025-05-20-100433723.png' // Lightning Obelisk; 6'2" lighting
+imageSrc: 'https://i.postimg.cc/PfKgj89S/image-2025-05-20-100521354.png' // Rage Beacon; ragussy
