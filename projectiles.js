4e7c0278750b2ea7c3bf1b8c0e481020ba42bbc8
Darsh220 <darsh.srivastava@gmail.com>
2025-05-21 22:25:15 -0700

    Bomb tower projectile added

--- projectiles.js
+++ projectiles.js
@@ -2,3 +2,4 @@
 
 imageSrc: 'https://i.postimg.cc/gjznhbcv/image-2025-05-21-114040090.png' // archer arrow
 imageSrc: 'https://i.postimg.cc/TwGw8vDZ/image-2025-05-21-114249663.png' // wizard fireball
+imageSrc: 'https://i.postimg.cc/L6qPCWkV/download-removebg-preview.png' // bomb tower projectile
