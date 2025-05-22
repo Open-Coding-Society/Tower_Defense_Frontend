dd6d3327e066080d843192897f90835a87536ad8
LarsLindain <andre.lindain@gmail.com>
2025-05-21 22:48:45 -0700

    projectiles

--- projectiles.js
+++ projectiles.js
@@ -3,3 +3,5 @@
 imageSrc: 'https://i.postimg.cc/gjznhbcv/image-2025-05-21-114040090.png' // archer arrow
 imageSrc: 'https://i.postimg.cc/TwGw8vDZ/image-2025-05-21-114249663.png' // wizard fireball
 imageSrc: 'https://i.postimg.cc/L6qPCWkV/download-removebg-preview.png' // bomb tower projectile
+imageSrc: 'https://i.postimg.cc/cLc8rtQv/image-2025-05-21-224441846.png' // inferno laser
+imageSrc: 'https://i.postimg.cc/4dY56Q5L/image-2025-05-21-224712958.png' // lightning bolt
