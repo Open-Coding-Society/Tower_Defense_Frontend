81d3830572cd84b0d2351b15a2092d1baaba7204
LarsLindain <andre.lindain@gmail.com>
2025-05-29 10:37:59 -0700

    projectile

--- projectiles.js
+++ projectiles.js
@@ -6,3 +6,6 @@ imageSrc: 'https://i.postimg.cc/cLc8rtQv/image-2025-05-21-224441846.png' // infe
 imageSrc: 'https://i.postimg.cc/4dY56Q5L/image-2025-05-21-224712958.png' // lightning bolt
 imageSrc: 'https://i.postimg.cc/L6qPCWkV/download-removebg-preview.png' // bomb tower projectile
 imageSrc: 'https://i.postimg.cc/hGVgS6Ng/image-2025-05-21-225231613.png' // magic beam
+imageSrc: 'https://i.postimg.cc/c1tDmc2t/image-2025-05-29-095416276.png' // hunter nest cannonball
+imageSrc: 'https://i.postimg.cc/k4vW1BQZ/image-2025-05-29-100856983.png' // freeze ice effect
+imageSrc: 'https://i.postimg.cc/4dYmty8V/image-2025-05-29-101044073.png' // lightning Obelisk projectile
