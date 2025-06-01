05db191f87b22e377b964051741d344e7f1cf102
Prad-coder <pradyungowda@gmail.com>
2025-05-31 22:37:24 -0700

    port

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,10 +1,10 @@
 export var pythonURI;
 if (location.hostname === "localhost") {
-        pythonURI = "http://localhost:8887";
+        pythonURI = "http://localhost:3168";
 } else if (location.hostname === "127.0.0.1") {
-        pythonURI = "http://127.0.0.1:8887";
+        pythonURI = "http://127.0.0.1:3168";
 } else {
-        pythonURI =  "https://flask2025.nighthawkcodingsociety.com";
+        pythonURI =  "https://tower-defense.opencodingsociety.com";
 }
 export var javaURI;
 if (location.hostname === "localhost") {
